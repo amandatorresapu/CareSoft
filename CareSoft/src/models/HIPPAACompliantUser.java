1027be243416812e321d3035c2e79dc659bd879c
Amanda Torres <69279617+amandatorresapu@users.noreply.github.com>
2022-04-07 13:23:20 -0700

    Add files via upload

---
+++ CareSoft/src/models/HIPPAACompliantUser.java
@@ -0,0 +1,15 @@
+package models;
+
+public interface HIPPAACompliantUser {
+
+	abstract boolean assignPin(int pin);
+	
+	abstract boolean accessAuthorized(Integer confirmedAuthID);
+	
+	
+//	abstract boolean assignPinAdmin(Integer confirmedAuthI);
+
+
+
+}
+
